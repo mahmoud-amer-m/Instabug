b8c2de33de1270935fbd3da4dc628e22e375811e
Mahmoud Amer <mahmoud.amer.m@gmail.com>
2018-03-14 21:32:54 +0200

    Adding Example Project

--- Example/Pods/Target Support Files/InstaNetworking/InstaNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_InstaNetworking : NSObject
-@end
-@implementation PodsDummy_InstaNetworking
-@end
--- Example/Pods/Target Support Files/Pods-InstaNetworking_Example/Pods-InstaNetworking_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InstaNetworking_Example : NSObject
-@end
-@implementation PodsDummy_Pods_InstaNetworking_Example
-@end
